8f18d9293e6936e40c4b62e0602f8e02549e0770
Nikit Hamal <iamnikithamal@gmail.com>
2026-04-10 04:57:06 +0545

    fix: add Sora editor deps back + fix getChangeSet ambiguity in ProjectBuilder

--- app/src/main/java/a/a/a/ProjectBuilder.java
+++ app/src/main/java/a/a/a/ProjectBuilder.java
@@ -677,7 +677,7 @@ public class ProjectBuilder {
         );
 
         IncrementalCompileCache compileCache = new IncrementalCompileCache(yq.sc_id, "java");
-        IncrementalCompileCache.ChangeSet changeSet = compileCache.getChangeSet(
+        IncrementalCompileCache.ChangeSet changeSet = compileCache.getChangeSetWithEnvironment(
                 buildJavaCompilationEnvironmentFingerprint(),
                 normalizedGeneratedPath,
                 yq.rJavaDirectoryPath,
